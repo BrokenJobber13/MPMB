5e3f9f2b499c70b83a5dca602bf506ffe8eb5f82
BrokenJobber13 <74739204+BrokenJobber13@users.noreply.github.com>
2020-11-21 20:46:08 +0000

    Create Half Taxabi.js

---
+++ Half Taxabi.js
@@ -0,0 +1,35 @@
+var iFileName = "Hald Taxabi.js"; 
+RequiredSheetVersion(12.999); 
+
+RaceList["half taxabi"] = { 
+
+	regExpSearch : /half taxabi/i, 
+
+	name : "Half Taxabi", 
+
+	source : ["HB", 0], 
+
+	plural : "Half Taxabi", 
+	size : 3, 
+	speed : { 
+		walk : { spd : 30, enc : 20 }, 
+
+		climb : { spd : "20", enc : 10 }, 	},
+	toolProfs : ["Artisan's Tools", 2], 
+	languageProfs : ["Common", 1], 
+	weapons : ["claws"], 
+	vision : ["Darkvision", 60], 
+	
+	savetxt : { 
+		text : ["Investigating places,objects,people", "Perception checks that rely on hearing"], 	},
+
+	skills : ["Perception"], 
+	skillstxt : "Choose Intimidiation or Persuasion and another skill.",  
+	age : " reach adulthood in their late teens and live around 100 years", 
+	height : " range from 5 to over 6 feet tall (4'9\" + 2d8\")", 
+	weight : " weigh around 55 lb (49 + 2d8 \xD7 2d4 lb)", 
+	improvements : "Half Taxabi: +1 Dexterity, +1 Free Choice, +1 Free Choice;", 
+	scores : [0, 1, 0, 0, 0, 0], 
+	trait : "Half Taxabi (+1 Dexterity, +1 Free Choice, +1 Free Choice)\n  .", 
+
+};
