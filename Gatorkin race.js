8e78419c8e696f3da0187765e1eb50bad4046032
BrokenJobber13 <74739204+BrokenJobber13@users.noreply.github.com>
2020-11-21 02:02:04 +0000

    Update Gatorkin race.js

--- Gatorkin race.js
+++ Gatorkin race.js
@@ -10,18 +10,18 @@ RaceList["gatorkin"] = {
 	size : 3,
 	speed : {
 		walk : { spd : 30, enc : 20 },
-		swim : { spd : 30, enc : 20 }
+		swim : { spd : 40, enc : 30 }
 	},
-	skillstxt : "Choose two from Animal Handling, Nature, Perception, Stealth, and Survival",
+	skillstxt : "Choose any two skills",
 	languageProfs : ["Draconic", 1],
 	weaponOptions : {
-		baseWeapon : "unarmed strike",
-		regExpSearch : /\bbite\b/i,
-		name : "Bite",
-		source : ["V", 113],
+		baseWeapon : "sharks greater bite",
+		regExpSearch : /sharks greater bite/i,
+		name : "Sharks Greater Bite",
+		source : ["HB", 0],
 		damage : [2, 6, "piercing"]
 	},
-	weaponsAdd : ["Bite"],
+	weaponsAdd : ["Sharks Greater Bite"],
 	armorOptions : {
 		regExpSearch : /^(?=.*natural)(?=.*armou?r).*$/i,
 		name : "Natural Armor",
@@ -29,20 +29,20 @@ RaceList["gatorkin"] = {
 		ac : 13
 	},
 	armorAdd : "Natural Armor",
-  	savetxt : { // Optional; this attribute defines entries to add to the field for "Saving Throw Advantages / Disadvantages"
-
-		text : ["Dex save vs. area effects: fail \u2015 half dmg, success \u2015 no dmg", "Magic can't put me to sleep"], // Optional; this is an array of strings, and each of those strings is added to the field exactly as presented here
-
-		immune : ["poison", "disease"], // Optional; this is an array of strings that the character is immune to. This is put in the field after the text "Immune to ", so in this example it would result in "Immune to poison and disease"
-
-		adv_vs : ["traps", "charmed"] // Optional; this is an array of things that the character has advantage on saves against. This is put in the field after the text "Adv. on saves vs. ", so in this example it would result in "Adv. on saves vs. traps and charmed"
-	},
+  	savetxt : { 
+		adv_vs : ["on all Str checks in water", "being frightened", "Stealth and hiding in water"] },
+		skills : ["Insight", "Deception"],
 	age : " reach maturity around age 14 and rarely live longer than 60 years",
 	height : " range from 5 to well over 6 feet tall (4'9\" + 2d10\")",
 	weight : " weigh around 200 lb (120 + 2d10 \xD7 2d6 lb)",
 	heightMetric : " range from 1,5 to well over 1,8 metres tall (145 + 5d10 cm)",
 	weightMetric : " weigh around 95 kg (55 + 5d10 \xD7 4d6 / 10 kg)",
-	scores : [0, 0, 2, 0, 1, 0],
+	
+	improvements : "Gatorkin : +1 Constitution, +1 Wisdom, +2 Strength;", 
+
+	scores : [2, 0, 1, 0, 1, 0],
+	trait : "Gatorkin (+2 Strength +1 Constitution, +1 Wisdom) Powerful jaws - My jaws cause more damage on impact and allow me as a Bonus action to grapple the target." + (typePF ? "\n" : " ") + "Cunning Artisan: As part of a short rest I can harvest parts of a slain creature to make a shield, club, javelin, or 1d4 darts/blowgun needles. This requires a suitable corpse and appropriate tools." + (typePF ? "\n" : " ") + "Hold Breath: I can hold my breath for up to 30 minutes at a time." + (typePF ? "\n" : " ") + "Natural Armor: I have an AC of 13 + Dexterity modifier + shield." + (typePF ? "\n" : " ") "
+	abilitySave : 1,  
 	features : {
 		"cunning artisan" : {
 			name : "Cunning Artisan",
@@ -50,13 +50,12 @@ RaceList["gatorkin"] = {
 			usages : 1,
 			recovery : "short rest"
 		},
-		"hungry jaws" : {
-			name : "Hungry Jaws",
+		"grapple biten" : {
+			name : "Grapple Biten",
 			minlevel : 1,
-			usages : 1,
-			recovery : "short rest",
+			recovery : "long rest",
 			action : ["bonus action", ""]
 		}
 	},
-	trait : "Lizardfolk (+2 Constitution, +1 Wisdom) Bite: I can use my fanged maw to make unarmed strikes dealing 1d6 piercing damage." + (typePF ? "\n" : " ") + "Cunning Artisan: As part of a short rest I can harvest parts of a slain creature to make a shield, club, javelin, or 1d4 darts/blowgun needles. This requires a suitable corpse and appropriate tools." + (typePF ? "\n" : " ") + "Hold Breath: I can hold my breath for up to 15 minutes at a time." + (typePF ? "\n" : " ") + "Natural Armor: I have an AC of 13 + Dexterity modifier + shield." + (typePF ? "\n" : " ") + "Hungry Jaws: As a bonus action, once per short rest, I can make a special bite attack and if it hits I gain temporary HP equal to my Con modifier (min 1)."
+	
 };
