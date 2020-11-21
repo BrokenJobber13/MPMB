32664708d0622c47ba9ceffabf1c4957eacef0f3
BrokenJobber13 <74739204+BrokenJobber13@users.noreply.github.com>
2020-11-21 02:18:11 +0000

    Create Lustrous.js

---
+++ Lustrous.js
@@ -0,0 +1,34 @@
+T rained by illusions and made of gems the Lustrous are aphysically strong race.
+Abi l i ty  Score  Increase.  Y our Constitution score increasesby 2 .
+Age.  Lustrous are born fully grown. As constructs, barringbeing physically destroyed , they do not age.
+Al i gnment.  Lustrous are usually of neutral alignment asthey normally only act when ordered and have no personalityto move them otherwise.
+Speed .  Y our base walking speed is 30 feet.
+Il lusi onar y  T raining.  Y ou gain proficiency in one weapon,tool , or skill of your choice.
+Rock  Born.  Y ou were born of rock which affords you apowerful constitution.
+Y ou have advantage on saving throws against beingpoisoned .
+Y ou are immune to disease.
+Y ou don 't need to eat, drink, or breathe.
+Y ou don 't need to sleep and don 't suffer the effects ofexhaustion due to lack of rest, and magic can ’ t put you tosleep. However , you do need light rest for 4 hours to gainthe benefits of a long rest.
+Subrace.  Different types of gems have different properties.
+Choose between the T enacious, Hardy , or Stable gems.
+
+
+Tenacious.
+T enacious lustres are those that are physically tougher thanothers but less brilliant. T enacious lustres are those made ofgems like nephrite and jadeite.
+Abi l i ty  Score  Increase.  Y our Strength score increases by 1 .
+Unbreakabl e.  Y ou do double damage when damaginginanimate objects with a weapon or natural attacks.
+T ougher  Stuf f.  Y our hit point maximum increases by 1 , andit increases by 1 every time you gain a level .
+
+
+ardy
+Hardy lustres are created to be leaders and are some of thehardest gems around . A fact they use to their advantage.Hardy lustres are made out of gems like diamond andcorundum.
+Abi l i ty  Score  Increase.  Y our Charisma score increases by1 .
+The Hardest  Lustres.  Y ou have learned to take advantageof your extremely hard body . When you take damage, you canuse your reaction to roll a d8 adding your Constitutionmodifier to the number rolled . Y ou reduce the damage takenby that amount. Y ou can use this ability a number of timesequal to your proficiency bonus. Y ou regain all uses after along rest.
+Born  to  Lead .  Y ou gain proficiency with the Persuasionskill . Y ou also have advantage on any checks made to interactwith military personnel . E.g. Soldiers, Guards, etc.
+
+
+Stable.
+Stable lustres are the most resistant to chemical changes intheir composition. This and their training during their growthaffords them extra talent with the magic around them. Stablelustres are made out of gems like alexandrites and garnets.
+Abi l i ty  Score  Increase.  Y our Wisdom score increases by 1 .
+Balanced  Composi ti on. Y ou gain resistance to one damagetype. Choose from acid , cold , fire, or lightning
+.Earthbound Mani pulati on.  Y ou know the Mold Earthcantrip. Once you reach 3rd level , you can cast the EarthT remor spell once as a 1st-level spell . Y ou must finish a longrest to cast these spells again with this trait. Wisdom is yourspellcasting ability for these spells
