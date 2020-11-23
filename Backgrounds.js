eeca92f230436b8e992ddf2def769fc2ca6054df
BrokenJobber13 <74739204+BrokenJobber13@users.noreply.github.com>
2020-11-23 16:41:08 +0000

    Update Backgrounds.js

--- Backgrounds.js
+++ Backgrounds.js
@@ -1,25 +1,17 @@
-var iFileName = "Backgrounds.js"; // Optional; This is how the file will be named in the sheet if you import it as a file and not copy-paste its content. Only the first occurrence of this variable will be used
-RequiredSheetVersion(12.999); // Optional; This is the minimum required version number of the sheet for the script to work. If the sheet being used to import the script is of an earlier version, the user will be warned
-
-BackgroundList["advisor"] = { //Object name; Note the use of only lower case! Also note the absence of the word "var" and the use of brackets []
-	regExpSearch : /Advisor/i, //required; regular expression of what to look for (i.e. now it looks for any entry that has the consecutive words "where", "i", "am", and "from" in it, disregarding capitalization or words in between). If this looks too complicated, just write: /where i am from/i
-
-	name : "Advisor", //required; the name as used
-
-	source : ["HB", 0], //required; the source and the page number. "HB" stands for homebrew. See the "Complete SourceList" for an overview of sources that are already defined. Or define a new source using the "Homebrew Syntax - SourceList.js". // This can be an array of arrays to indicate the things appears in multiple sources. For example, if something appears on page 7 of the Elemental Evil Player's Companion and on page 115 of the Sword Coast Adventure Guide, use the following: [["E", 7], ["S", 115]]
-
-	skills : ["History", "Persuasion"], //optional; skill proficiencies gained from having the background. If the background doesn't give fixed proficiencies, but instead gives a choice, delete this line and use the line below, "skillstxt"
-
-	gold : 15, //required; the amount of gold pieces added to the Equipment section on the second page when selecting "Background's items and gold" from the "Add Equipment" menu.
-
-	equipleft : [ //optional; syntax is: ["description", "amount", "weight"]. Put "" if it is nothing, don't put 0
+var iFileName = "Backgrounds.js"; 
+RequiredSheetVersion(12.999); 
+BackgroundList["advisor"] = { 
+	regExpSearch : /Advisor/i, 
+	name : "Advisor", 
+	source : ["HB", 0], 
+	skills : ["History", "Persuasion"], 
+	gold : 15, 
+	equipleft : [ 
 		["A Bottle of ink and Quill", "", ""],
 		["Pieces of Parchment", "5", ""],
 		["Set of Fine Clothes", "", "2"],
-	], //items as they are added to the left column of the Equipment section on the second page when selecting "Background's items and gold" from the "Add Equipment" menu.
-
-	feature : "Position of Power", //required; the name of the background feature as it will appear on the sheet. The feature is then retrieved from the BackgroundFeatureList, see below
-
+	], 
+	feature : "Position of Power", 
 	trait : [
 		"I am convinced that my beliefs are the correct ones. Anyone who believes differently is at best mistake.",
 		"I enjoy when people recognize my importance.",
@@ -28,9 +20,8 @@ BackgroundList["advisor"] = { //Object name; Note the use of only lower case! Al
 		"I never forget a debt owed to me, nor one I owe to someone else.",
 		"It is my responsibility to make sure my friends are taken care of however I can.",
 		"I consder it my duty to educate those who are iignorant of something I am knowledgeable about.",
-		"I always conduct myself formally and with dignity."
-	], //required; A list of the personality traits that can be chosen using the "Add Features" button on the second page. This list can be any length.
-
+		"I always conduct myself formally and with dignity.",
+	], 
 	ideal : [
 		["Tradition",
 			"Tradition: The best advice I can provide is to keep to the laws and traditions of the past. (Lawful)"
@@ -50,8 +41,7 @@ BackgroundList["advisor"] = { //Object name; Note the use of only lower case! Al
 		["Practicality",
 			"Practicality: Always take the most effective route toward accomplishing ones goals. (Neutral)"
 		],
-	], //required; A list of the  ideals that can be chosen using the "Add Features" button on the second page. This list can be any length. Take note of the two-step build for every ideal, this is essential!
-
+	], 
 	bond : [
 		"The company I am part of is all that matters to me. My role is to serve it as best I can.",
 		"I admire and respect my superior, and I will gladly serve him or her or as long as they will have me.",
@@ -59,8 +49,7 @@ BackgroundList["advisor"] = { //Object name; Note the use of only lower case! Al
 		"I was put in this position by my mentor, my superior'spredecessor. I miss my mentor dearly.",
 		"People will know my name before I'm through with this world.",
 		"This position is a means to an end. Someday, it will be my turn to rule."
-	], //required; A list of the bonds that can be chosen using the "Add Features" button on the second page. This list can be any length.
-
+	], 
 	flaw : [
 		"I advise rather than lead because I'm unwilling to make hard decisions myself.",
 		"My advice once caused catastrophic consequences, but he does not realise. I'll do anything to keep him from knowing.",
@@ -68,8 +57,7 @@ BackgroundList["advisor"] = { //Object name; Note the use of only lower case! Al
 		"I can't help myself when I learn something secret. I simply must tell someone.",
 		"I am not afriad to use my position of power if it means I can get the things I want.",
 		"I secretly believe that I should be the one in power, not my superior."
-	],  //required; A list of the bonds that can be chosen using the "Add Features" button on the second page. This list can be any length.
-
+	],  
 	extra : [
 		"Select a Advisor Specialty",
 		"Council",
@@ -81,22 +69,18 @@ BackgroundList["advisor"] = { //Object name; Note the use of only lower case! Al
 		"City Guard",
 		"Treasurery",
 		"Spy"
-	], //optional; the extra options the background gives on the first page of the sheet (in line Background at the top there are two drop-down menus). The first entry in this array is what is used for the mouseover text. If your background offers no extra features, simply delete this entry. Make sure that text you enter here fits into the field, or it won't look as good
-
-/* SYNTAX CHANGE v12.998 >> old syntax for 'tools' and 'languages' are no longer supported!! */
-	toolProfs : [["", 0], ["Forgery Kit", "Int"]], // optional; this is an array with the tool proficiencies gained. Each entry can be its own array of 2 entries. The first entry is the name of the tool and the second entry is either 1) a number if the tool is yet to be chosen, or 2) the 3-letter ability score abbreviation if the tool is to be listed in the skill section and have a bonus calculated
-
-	languageProfs : [2], // optional; this is an array of the language proficiencies gained. An entry can either be 1) a string that represents the language learned or 2) a number which is the number of language gained that can be chosen by the player
-
-	lifestyle : "aristocratic", //optional; sets the lifestyle of the sheet. Options are: "wretched", "squalid", "poor", "modest", "comfortable", "wealthy", or "aristocratic"
+	], 
+	toolProfs : [["", 0], ["Forgery Kit", "Int"]], 
+	languageProfs : [2], 
+	lifestyle : "aristocratic", 
 };
 
-BackgroundFeatureList["position of power"] = {  //Note the use of only lower case!
+BackgroundFeatureList["position of power"] = {  
 	description : "Because of your position, you command a certain respect. People who both know who you are and also have a vested interested in pleasing your liege tend to give you the benefit of the doubt and will go out of their way not to draw your ire, lest they potentially incur the wrath of your superior. Others within the organization you are a part of are also likely to curry favor from you however they can. Beware, however, as there could also be some close to you who are jealous of your position and may wish for your downfall.", //required; the description of the feature as it will be put on the sheet. Make sure that this fits into the field or it won't look so pretty.
-
+};
 
 BackgroundList["demon hunter"] = { 
-	regExpSearch : /demon hunter/i, 
+	regExpSearch : /Demon Hunter/i, 
 	name : "Demon Hunter", 
 	source : ["HB", 0], 
 	skills : ["Religion", "Survival"], 
@@ -194,7 +178,7 @@ BackgroundList["doctor"] = {
 	source : ["HB", 0], 
 	skills : ["Persuasion", "Medicine"], 
 	gold : 15, 
-	equipleft : [ //optional; syntax is: ["description", "amount", "weight"]. Put "" if it is nothing, don't put 0
+	equipleft : [ 
 		["Set of doctors tools", "", "1"],
 		["healers kit", "", 2],
 	], 
@@ -204,7 +188,7 @@ BackgroundList["doctor"] = {
 		["Belt pouch (with coins)", "", 1],
 	],
 
-	feature : "House Calls", //required; the name of the background feature as it will appear on the sheet. The feature is then retrieved from the BackgroundFeatureList, see below
+	feature : "House Calls", 
 
 	trait : [
 		"I am sympathetic to anyone in pain",
@@ -254,8 +238,7 @@ BackgroundList["doctor"] = {
 		"I once made a mistake that cost a patient their life. I must make sure that no one finds out.",
 		"I have a tendency to see my patients as opportunities rather than people.",
 		"I am hesitant to injure others, even in self-defense."
-	],  //required; A list of the bonds that can be chosen using the "Add Features" button on the second page. This list can be any length.
-
+	],  
 	extra : [
 		"Select a Doctor Specialty",
 		"Apothecary",
@@ -396,8 +379,7 @@ BackgroundList["fairy tale child"] = {
 		"I brush my hair 100 times before bed.",
 		"I fuss over those I care about, wanting them to look their best",
 		"I blow up at the slightest insult."
-	], //required; A list of the personality traits that can be chosen using the "Add Features" button on the second page. This list can be any length.
-
+	], 
 	ideal : [
 		["Optimism",
 			"Optimism : Every cloud has a silver lining. (Any)"
@@ -452,21 +434,18 @@ BackgroundFeatureList["exceptional quality"] = {
 };
 
 
-BackgroundList["dragon slayer"] = { //Object name; Note the use of only lower case! Also note the absence of the word "var" and the use of brackets []
-	regExpSearch : /dragon slayer/i, //required; regular expression of what to look for (i.e. now it looks for any entry that has the consecutive words "where", "i", "am", and "from" in it, disregarding capitalization or words in between). If this looks too complicated, just write: /where i am from/i
-
-	name : "Dragon Slayer", //required; the name as used
-
-	source : ["HB", 0], //required; the source and the page number. "HB" stands for homebrew. See the "Complete SourceList" for an overview of sources that are already defined. Or define a new source using the "Homebrew Syntax - SourceList.js". // This can be an array of arrays to indicate the things appears in multiple sources. For example, if something appears on page 7 of the Elemental Evil Player's Companion and on page 115 of the Sword Coast Adventure Guide, use the following: [["E", 7], ["S", 115]]
-
+BackgroundList["dragon slayer"] = { 
+	regExpSearch : /dragon slayer/i, 
+	name : "Dragon Slayer", 
+	source : ["HB", 0], 
 	skills : ["Survival", "Stealth"], 
 	gold : 15, 
 
-	equipleft : [ //optional; syntax is: ["description", "amount", "weight"]. Put "" if it is nothing, don't put 0
+	equipleft : [ 
 		["Marked Map", "", ""],
 	], 
 
-	equipright : [ //optional; samy syntax as "equipleft"
+	equipright : [ 
 		["Traveller's clothes", "", 3],
 		["Belt pouch (with coins)", "", 1],
 	],
