c7157375bc2832e77db8efddbe07a8b897536cca
BrokenJobber13 <74739204+BrokenJobber13@users.noreply.github.com>
2020-11-23 13:06:39 +0000

    Update Adventure Gear.js

--- Adventure Gear.js
+++ Adventure Gear.js
@@ -1,17 +1,12 @@
 var iFileName = "Adventure Gear.js"; // Optional; This is how the file will be named in the sheet if you import it as a file and not copy-paste its content. Only the first occurrence of this variable will be used
 RequiredSheetVersion(12.999); // Optional; This is the minimum required version number of the sheet for the script to work. If the sheet being used to import the script is of an earlier version, the user will be warned
 
-GearList["basic bomb"] = { //Object name; Note the use of only lower case! Also note the absence of the word "var" and the use of brackets []. //The name here is used to sort the gear alphabetically.
-
-	infoname : "Basic Bomb [150 gp]", //Required; This is the name as it will appear in the equipment menu
-
-	name : "Basic Bomb", //Required; The name as it will be put into the equipment table
-
-	amount : 1, //Required; The amount as will be put into the equipment table. You can put "" here if you like
-
-	weight : 1, //Required, has to be a number; The weight as will be put into the equipment table. Note that the total weight is calculated as Amount×Weight. You can put "" here if you like
-
-	source : ["HB", 0] //Optional; the source and the page number. "HB" stands for homebrew. See the "Complete SourceList" for an overview of sources that are already defined. Or define a new source using the "Homebrew Syntax - SourceList.js". //If you don't define this, the equipment will always be available, regardless of what sources are excluded/included // This can be an array of arrays to indicate the things appears in multiple sources. For example, if something appears on page 7 of the Elemental Evil Player's Companion and on page 115 of the Sword Coast Adventure Guide, use the following: [["E", 7], ["S", 115]]
+GearList["basic bomb"] = { 
+	infoname : "Basic Bomb [150 gp]", 
+	name : "Basic Bomb", 
+	amount : 1, 
+	weight : 1, 
+	source : ["HB", 0] 
 };
 
 GearList["grooming kit, animal"] = { 
@@ -30,6 +25,7 @@ GearList["grappling bolts"] = {
 	name : "Grappling Bolts",
 	amount : 5, 
 	weight : 1, 
+	type : "Ammunitition",
 	source : ["HB", 0]
 };
 
@@ -40,6 +36,17 @@ GearList["grappling arrows"] = {
 	name : "Grappling Arrows",
 	amount : 5, 
 	weight : 1, 
+	type : "Ammunitition",
+	source : ["HB", 0]
+};
+
+GearList["black powder small keg"] = { 
+
+	infoname : " Black Powder, Small Keg [5 gp]",
+	name : "Blck Powder, Small Keg",
+	amount : 80, 
+	weight : 8, 
+	type : "Ammunitition",
 	source : ["HB", 0]
 };
 
@@ -49,6 +56,7 @@ GearList["gunpowder small keg"] = {
 	name : "Gunpowder, Small Keg",
 	amount : 1, 
 	weight : 10, 
+	type : "Ammunitition",
 	source : ["HB", 0]
 };
 
@@ -67,6 +75,7 @@ GearList["eye patch"] = {
 	name : "Eye Patch",
 	amount : 1, 
 	weight : "", 
+	type : "Clothes",
 	source : ["HB", 0]
 };
 
@@ -76,6 +85,7 @@ GearList["pirate captain hat"] = {
 	name : "Pirate Captain's Hat",
 	amount : 1, 
 	weight : 2, 
+	type : "Clothes",
 	source : ["HB", 0]
 };
 
@@ -85,6 +95,7 @@ GearList["pirates hat"] = {
 	name : "Pirate's Hat",
 	amount : 1, 
 	weight : 1, 
+	type : "Clothes",
 	source : ["HB", 0]
 };
 
@@ -94,6 +105,7 @@ GearList["nautical chart"] = {
 	name : "Nautical Chart",
 	amount : 1, 
 	weight : "", 
+	type : "Nautical",
 	source : ["HB", 0]
 };
 
@@ -103,5 +115,6 @@ GearList["sextant"] = {
 	name : "Sextant",
 	amount : 1, 
 	weight : 2, 
+	type : "Nautical",
 	source : ["HB", 0]
 };
