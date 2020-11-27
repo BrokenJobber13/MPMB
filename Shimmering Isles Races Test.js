ba4755418d73b02cb28533fc43b27a23e66a8640
BrokenJobber13 <74739204+BrokenJobber13@users.noreply.github.com>
2020-11-27 20:04:46 +0000

    Update Shimmering Isles Races Test.js

--- Shimmering Isles Races Test.js
+++ Shimmering Isles Races Test.js
@@ -635,6 +635,7 @@ RaceList["taintblood demon"] = {
     },		
 },
 },
+},
 
 RaceList["winterborn human"] = { 
 	regExpSearch : /^(?=.*winterborn)(?=.*human).*$/i, 
@@ -851,13 +852,13 @@ RaceList["gatorkin"] = {
 			range : "Melee",
 			description : "Natural, Lunge 10ft, Finesse, Brutal Reroll 1's.",
 			abilitytodamage : true, },
+  	armorAdd : "Natural Armor",
 	armorOptions : {
 		regExpSearch : /^(?=.*natural)(?=.*armou?r).*$/i,
 		name : "Natural Armor",
 		source : ["V", 113],
 		ac : 15
 	},
-	armorAdd : "Natural Armor",
   	savetxt : { 
 		adv_vs : ["on all Str checks in water", "being frightened", "Stealth and hiding in water"], },
   skills : ["Insight", "Deception"],
@@ -866,9 +867,7 @@ RaceList["gatorkin"] = {
 	weight : " weigh around 200 lb (120 + 2d10 \xD7 2d6 lb)",
 	heightMetric : " range from 1,5 to well over 1,8 metres tall (145 + 5d10 cm)",
 	weightMetric : " weigh around 95 kg (55 + 5d10 \xD7 4d6 / 10 kg)",
-	
 	improvements : "Gatorkin : +1 Constitution, +1 Wisdom, +2 Strength;", 
-
 	scores : [2, 0, 1, 0, 1, 0],
 	trait : "Gatorkin (+2 Strength +1 Constitution, +1 Wisdom)\nPowerful jaws - My jaws cause more damage on impact and allow me as a Bonus action to grapple the target.\nCunning Artisan: As part of a short rest I can harvest parts of a slain creature to make a shield, club, javelin, or 1d4 darts/blowgun needles.\nThis requires a suitable corpse and appropriate tools.\nHold Breath\nI can hold my breath for up to 30 minutes at a time.\nNatural Armor: I have an AC of 13 + Dexterity modifier + shield.",
 	abilitySave : 1,  
@@ -877,8 +876,8 @@ RaceList["gatorkin"] = {
 			name : "Cunning Artisan",
 			minlevel : 1,
 			usages : 1,
-			recovery : "short rest"
-		},
+			recovery : "short rest",
+      action : ["bonus action", ""]	},
 		"grapple biten" : {
 			name : "Grapple Biten",
 			minlevel : 1,
@@ -886,8 +885,9 @@ RaceList["gatorkin"] = {
 			action : ["bonus action", ""]
 		},
 	},
-	
+
 },
+
 
 
 WeaponsList["gators greater bite"] = {
