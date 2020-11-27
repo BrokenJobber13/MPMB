237b76384bf4bd180888e1a1b83dc1e866991eeb
BrokenJobber13 <74739204+BrokenJobber13@users.noreply.github.com>
2020-11-27 20:16:56 +0000

    Update Shimmering Isles Races Test.js

--- Shimmering Isles Races Test.js
+++ Shimmering Isles Races Test.js
@@ -1,6 +1,143 @@
 var iFileName = "Shimmering Isles Races Test.js"; 
 RequiredSheetVersion(12.999); 
 
+
+RaceList["Taintblood Devil"] = { 
+	regExpSearch : /Taintblood Devil/i, 
+	name : "Taintblood Devil", 
+	source : ["HB", 0], 
+	plural : "Taintblood's", 
+	size : 3, 
+	speed : { 
+		walk : { spd : 30, enc : 20 }, 	},
+	languageProfs : ["Common", "Infernal"], 
+	vision : ["Darkvision", 30], 
+	dmgres : ["Poison"], 
+	savetxt : { 
+		text : ["Adv on social checks with demons.", "Disadv on social checks demons make on me."], 	},
+	age : " A taintblood lives for about two centuries, reaching maturity at the age of 20", 
+	height : " range from 5 to over 7 feet tall (4'9\" + 2d8\")", 
+	weight : " weigh around 155 lb (110 + 2d8 \xD7 2d4 lb)", 
+	improvements : "Taintblood Devil: +2 Dexterity, +1 Charisma;", 
+	scores : [0, 2, 0, 0, 0, 1], 
+	trait : "Taintblood Devil (+2 Dexterity, +1 Charisma)\nHeritage:\nI bear a mark on my body from my evil parent\nMask of the Taint:\nI can make minor changes to my looks like having normal eyes, hide teeth to appear human.\nPoisonous Veins: If a creature consumes my blood, it takes a Con save throw DC8+Prof+Spellcasting ability mod.\n on fail is Poisoned for 1min, also 2d6 damage a start of turn, target can repeat at end of their turn.", 
+	abilitySave : 5,  
+},
+
+RaceList["taintblood demon"] = { 
+	regExpSearch : /Taintblood Demon/i, 
+	name : "Taintblood Demon", 
+	source : ["HB", 0], 
+	plural : "Taint's", 
+	size : 3, 
+	speed : { 
+		walk : { spd : 30, enc : 20 }, 	},
+	languageProfs : ["Common", "Abyssal"], 
+	vision : ["Darkvision", 30], 
+	dmgres : ["Fire", "Radiant"], 
+	savetxt : { 
+		text : ["Adv social checks on demons", "Disadv on social checks by infernals"], 	},
+	age : " A taintblood lives for about two centuries, reaching maturity at the age of 20", 
+	height : " range from 5 to over 7 feet tall (4'9\" + 2d8\")", 
+	weight : " weigh around 155 lb (110 + 2d8 \xD7 2d4 lb)", 
+	improvements : "Taintblood Demon: +1 Charisma, +2 Strength;", 
+	scores : [1, 0, 0, 0, 0, 1], 
+	trait : "Taintblood Demon (+1 Charisma, +2 Strength)\nHeritage - I bear a mark on my body from my evil parent\nMask of the Taint\nI can make minor changes to my looks like having normal eyes, hide teeth to appear human.\nDemonic Flames\nWhen I deal Fire damage, it ignores as resistances but not immunities.", 
+	abilitySave : 5,  
+	spellcastingAbility : 6, 
+
+	features : { 
+		"fire bolt" : { 
+			name : "Fiery Veins (Level 1)", 
+      			limfeaname : "Fire Bolt",
+			minlevel : 1, 
+			tooltip : "", 
+			spellcastingBonus : { 
+				name : "Fiery Veins (level 1)",
+				spells : ["fire bolt"],
+				selection : ["fire bolt"],
+				atwill : true,
+			},
+    },		
+	},
+},
+
+RaceList["gatorkin"] = {
+	regExpSearch : /gatorkin/i,
+	name : "Gatorkin",
+	source : ["HB", 0],
+	plural : "Gatorkin",
+	size : 3,
+	speed : {
+		walk : { spd : 30, enc : 20 },
+		swim : { spd : 40, enc : 30 }
+	},
+	skillstxt : "Choose any two skills",
+	languageProfs : ["Draconic", 1],
+	weaponsAdd : ["Gators Greater Bite"],
+		
+			weaponOptions : {
+			regExpSearch : /Gators Greater Bite/i,
+			name : "Gators Greater Bite",
+			source : ["HB", 0],
+			ability : 1,
+			type : "Natural",
+			damage : [2, 6, "piercing"],
+			range : "Melee",
+			description : "Natural, Lunge 10ft, Finesse, Brutal Reroll 1's.",
+			abilitytodamage : true, },
+	armorAdd : "Natural Armor",
+	armorOptions : {
+		regExpSearch : /^(?=.*natural)(?=.*armou?r).*$/i,
+		name : "Natural Armor",
+		source : ["V", 113],
+		ac : 15
+	},
+  	savetxt : { 
+		adv_vs : ["on all Str checks in water", "being frightened", "Stealth and hiding in water"] },
+		skills : ["Insight", "Deception"],
+	age : " reach maturity around age 14 and rarely live longer than 60 years",
+	height : " range from 5 to well over 6 feet tall (4'9\" + 2d10\")",
+	weight : " weigh around 200 lb (120 + 2d10 \xD7 2d6 lb)",
+	heightMetric : " range from 1,5 to well over 1,8 metres tall (145 + 5d10 cm)",
+	weightMetric : " weigh around 95 kg (55 + 5d10 \xD7 4d6 / 10 kg)",
+	
+	improvements : "Gatorkin : +1 Constitution, +1 Wisdom, +2 Strength;", 
+
+	scores : [2, 0, 1, 0, 1, 0],
+	trait : "Gatorkin (+2 Strength +1 Constitution, +1 Wisdom) Powerful jaws - My jaws cause more damage on impact and allow me as a Bonus action to grapple the target.\nCunning Artisan: As part of a short rest I can harvest parts of a slain creature to make a shield, club, javelin, or 1d4 darts/blowgun needles. This requires a suitable corpse and appropriate tools\nHold Breath: I can hold my breath for up to 30 minutes at a time.\nNatural Armor: I have an AC of 13 + Dexterity modifier + shield. ",  
+	features : {
+		"cunning artisan" : {
+			name : "Cunning Artisan",
+			minlevel : 1,
+			usages : 1,
+			recovery : "short rest"
+		},
+		"grapple biten" : {
+			name : "Grapple Biten",
+			minlevel : 1,
+			recovery : "long rest",
+			action : ["bonus action", ""]
+		},
+	},
+	
+};
+
+
+WeaponsList["gators greater bite"] = {
+	regExpSearch : /gators greater bite/i,
+	name : "Gators Greater Bite",
+	source : ["HB", 0],
+	list : "Natural",
+	ability : 1,
+	type : "Martial",
+	damage : [2, 6, "Piercing"],
+	range : "Melee",
+	description : "Natural, Lunge 10ft, Finesse, Brutal Reroll 1's.",
+	abilitytodamage : true,
+}; 
+
+
 RaceList["salt gnome"] = { 
 	regExpSearch : /Salt Gnome/i, 
 	name : "Salt Gnome", 
@@ -576,67 +713,6 @@ RaceList["succubus"] = {
 
   },
 
-RaceList["Taintblood Devil"] = { 
-	regExpSearch : /Taintblood Devil/i, 
-	name : "Taintblood Devil", 
-	source : ["HB", 0], 
-	plural : "Taintblood's", 
-	size : 3, 
-	speed : { 
-		walk : { spd : 30, enc : 20 }, 	},
-	languageProfs : ["Common", "Infernal"], 
-	vision : ["Darkvision", 30], 
-	dmgres : ["Poison"], 
-	savetxt : { 
-		text : ["Adv on social checks with demons.", "Disadv on social checks demons make on me."], 	},
-	age : " A taintblood lives for about two centuries, reaching maturity at the age of 20", 
-	height : " range from 5 to over 7 feet tall (4'9\" + 2d8\")", 
-	weight : " weigh around 155 lb (110 + 2d8 \xD7 2d4 lb)", 
-	improvements : "Taintblood Devil: +2 Dexterity, +1 Charisma;", 
-	scores : [0, 2, 0, 0, 0, 1], 
-	trait : "Taintblood Devil (+2 Dexterity, +1 Charisma)\nHeritage:\nI bear a mark on my body from my evil parent\nMask of the Taint:\nI can make minor changes to my looks like having normal eyes, hide teeth to appear human.\nPoisonous Veins: If a creature consumes my blood, it takes a Con save throw DC8+Prof+Spellcasting ability mod.\n on fail is Poisoned for 1min, also 2d6 damage a start of turn, target can repeat at end of their turn.", 
-	abilitySave : 5,  
-},
-
-RaceList["taintblood demon"] = { 
-	regExpSearch : /Taintblood Demon/i, 
-	name : "Taintblood Demon", 
-	source : ["HB", 0], 
-	plural : "Taint's", 
-	size : 3, 
-	speed : { 
-		walk : { spd : 30, enc : 20 }, 	},
-	languageProfs : ["Common", "Abyssal"], 
-	vision : ["Darkvision", 30], 
-	dmgres : ["Fire"], 
-	savetxt : { 
-		text : ["Adv social checks on demons", "Disadv on social checks by infernals"], 	},
-	age : " A taintblood lives for about two centuries, reaching maturity at the age of 20", 
-	height : " range from 5 to over 7 feet tall (4'9\" + 2d8\")", 
-	weight : " weigh around 155 lb (110 + 2d8 \xD7 2d4 lb)", 
-	improvements : "Taintblood Demon: +1 Charisma, +2 Strength;", 
-	scores : [1, 0, 0, 0, 0, 1], 
-	trait : "Taintblood Demon (+1 Charisma, +2 Strength)\nHeritage - I bear a mark on my body from my evil parent\nMask of the Taint\nI can make minor changes to my looks like having normal eyes, hide teeth to appear human.\nDemonic Flames\nWhen I deal Fire damage, it ignores as resistances but not immunities.", 
-	abilitySave : 5,  
-	spellcastingAbility : 6, 
-
-	features : { 
-		"fire bolt" : { 
-			name : "Fiery Blood (Level 1)", 
-      limfeaname : "fire bolt",
-			minlevel : 1, 
-			tooltip : "", 
-			spellcastingBonus : { 
-				name : "Fiery Blood (level 1)",
-				spells : ["fire bolt"],
-				selection : ["fire bolt"],
-				atwill : true,
-			},
-    },		
-},
-},
-},
-
 RaceList["winterborn human"] = { 
 	regExpSearch : /^(?=.*winterborn)(?=.*human).*$/i, 
 	name : "Winterborn Human", 
@@ -825,82 +901,6 @@ RaceList["seaborn human"] = {
 	improvements : "Seaborn Human: +1 Free Choice, +2 Charisma;", 
 	scores : [0, 0, 0, 0, 0, 2], 
 	trait : "Seaborn Human (+1 Free Choice, +2 Charisma)\nAppraiser\nYou have advantage on checks to determine the value and craftwork of items.\nNatural Trader\nyou have advantage on Charisma (Persuasion) checks when negotiating prices with others that aren't hostile toward you.", 
-
-								},
-
-RaceList["gatorkin"] = {
-	regExpSearch : /gatorkin/i,
-	name : "Gatorkin",
-	source : ["HB", 0],
-	plural : "Gatorkin",
-	size : 3,
-	speed : {
-		walk : { spd : 30, enc : 20 },
-		swim : { spd : 40, enc : 30 },
-	},
-	skillstxt : "Choose any two skills",
-	languageProfs : ["Draconic", 1],
-	weaponsAdd : ["Gators Greater Bite"],
-		
-			weaponOptions : {
-			regExpSearch : /Gators Greater Bite/i,
-			name : "Gators Greater Bite",
-			source : ["HB", 0],
-			ability : 1,
-			type : "Natural",
-			damage : [2, 6, "piercing"],
-			range : "Melee",
-			description : "Natural, Lunge 10ft, Finesse, Brutal Reroll 1's.",
-			abilitytodamage : true, },
-  	armorAdd : "Natural Armor",
-	armorOptions : {
-		regExpSearch : /^(?=.*natural)(?=.*armou?r).*$/i,
-		name : "Natural Armor",
-		source : ["V", 113],
-		ac : 15
-	},
-  	savetxt : { 
-		adv_vs : ["on all Str checks in water", "being frightened", "Stealth and hiding in water"], },
-  skills : ["Insight", "Deception"],
-	age : " reach maturity around age 14 and rarely live longer than 60 years",
-	height : " range from 5 to well over 6 feet tall (4'9\" + 2d10\")",
-	weight : " weigh around 200 lb (120 + 2d10 \xD7 2d6 lb)",
-	heightMetric : " range from 1,5 to well over 1,8 metres tall (145 + 5d10 cm)",
-	weightMetric : " weigh around 95 kg (55 + 5d10 \xD7 4d6 / 10 kg)",
-	improvements : "Gatorkin : +1 Constitution, +1 Wisdom, +2 Strength;", 
-	scores : [2, 0, 1, 0, 1, 0],
-	trait : "Gatorkin (+2 Strength +1 Constitution, +1 Wisdom)\nPowerful jaws - My jaws cause more damage on impact and allow me as a Bonus action to grapple the target.\nCunning Artisan: As part of a short rest I can harvest parts of a slain creature to make a shield, club, javelin, or 1d4 darts/blowgun needles.\nThis requires a suitable corpse and appropriate tools.\nHold Breath\nI can hold my breath for up to 30 minutes at a time.\nNatural Armor: I have an AC of 13 + Dexterity modifier + shield.",
-	abilitySave : 1,  
-	features : {
-		"cunning artisan" : {
-			name : "Cunning Artisan",
-			minlevel : 1,
-			usages : 1,
-			recovery : "short rest",
-      action : ["bonus action", ""]	},
-		"grapple biten" : {
-			name : "Grapple Biten",
-			minlevel : 1,
-			recovery : "long rest",
-			action : ["bonus action", ""]
-		},
-	},
-
-},
-
-
-
-WeaponsList["gators greater bite"] = {
-	regExpSearch : /Gators Greater Bite/i,
-	name : "Gators Greater Bite",
-	source : ["HB", 0],
-	list : "Natural",
-	ability : 1,
-	type : "Martial",
-	damage : [2, 6, "Piercing"],
-	range : "Melee",
-	description : "Natural, Lunge 10ft, Finesse, Brutal Reroll 1's.",
-	abilitytodamage : true,
 },
 
 RaceList["glacier dwarf"] = { 
