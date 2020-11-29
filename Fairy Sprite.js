3dfa7732399058d9a3069a2eb0d3bf1dc2962053
BrokenJobber13 <74739204+BrokenJobber13@users.noreply.github.com>
2020-11-29 23:02:10 +0000

    Update Fairy Sprite.js

--- Fairy Sprite.js
+++ Fairy Sprite.js
@@ -17,4 +17,29 @@ RaceList["fairy sprite"] = {
 	improvements : "Fairy Sprite: +1 Dexterity or Intelligence, +2 Free Choice;", 
 	scores : [0, 0, 0, 0, 0, 0], 
 	trait : "Fairy Sprite (+1 Dexterity or Intelligence, +2 Free Choice)\nShrink Objects: By concentrating for a minute, a fairy can shrink an object to tiny size and 1/8th its original weight. If a fairy loses an object for a minute, it reverts to its regular size. It likewise reverts a minute after the fairies death. This only works on unattended objects. Fixed objects like doors cannot be shrunk.\nSpiteful: By focusing on anger I can secrete a poison called Spite. Bonus action, I can coat a weapon or 3 pieces of ammo to deal additional damage equal to your level until your next turn. This ability recharges after a short rest.\nHeart Sight: Any number of times per day you may touch a creature to determine its emotional state. If the target fails a Charisma save DC=8+Prof+Cha. I discern the targets emotional state and intentions. Celestials, fiends and undead automatically fail. A creature that saves against this ability can't be affected for 24 hours.\nInvisibility: Once per long rest, you can cast invisibility on yourself, which requires concentration. The spell ends after an hour or until you attack or cast a spell or until your concentration ends.", 
+	features : { 	
+	"spiteful" : { 
+			name : "Spiteful", 
+			minlevel : 1, 
+			usages : 1, 
+			recovery : "short rest", 
+			tooltip : "", 
+			action : ["bonus action", ""], 
+			},
+		"heart sight" : { 
+			name : "Heart Sight", 
+			minlevel : 1, 
+			tooltip : "", 
+			action : ["action", ""], 
+			},
+		"invisibility" : { 
+			name : "Invisibility", 
+			minlevel : 1, 
+			usages : 1, 
+			recovery : "long rest", 
+			tooltip : "Requires Concentration", 
+			action : ["action", ""], 
+			},
+  },
 };
+    
