18aa2648c35209cea05f9c4279c04cdd0053faff
BrokenJobber13 <74739204+BrokenJobber13@users.noreply.github.com>
2020-11-30 00:18:40 +0000

    Update Houzi Wong.js

--- Houzi Wong.js
+++ Houzi Wong.js
@@ -11,13 +11,19 @@ RaceList["houzi wong"] = {
 		walk : { spd : 30, enc : 20 }, 
 		climb : { spd : 30, enc : 20 }, 	},
 	languageProfs : ["Common", "Sylvan"], 
-  weaponsadd : ["monkey paws"],
-   		weaponOptions : {
-		baseWeapon : "monkey paws",
-		regExpSearch : /Monkey Paws/i,
-		name : "Monkey Paws",
-		source : ["HB", 0],
-		damage : [1, 6, "bludgeoning"]},
+ 	weaponsAdd : ["Monkey Paws"],
+		
+			weaponOptions : {
+			regExpSearch : /Monkey Paws/i,
+			name : "Monkey Paws", 
+			source : ["HB", 0], 
+			list : "melee", 
+			ability : 2, 
+			type : "Natural", 
+			damage : [1, 6, "bludgeoning"], 
+			range : "Melee", 
+			description : "Natural", 
+			abilitytodamage : true, },
 	weaponprofs : [false, false, ["glaive", "halberd", "pike"]], 
 	skills : ["Deception"], 
 	age : "Houzi-Wong live longer lives than humans, reaching maturity at 15 years of age and living until about 350 years of age. They are born without fur like a human, but grow it in through their youth. As they reach 250 years, it begins to gray and any older than 300 tend to have entirely gray fur on their bodies.", 
@@ -29,7 +35,7 @@ RaceList["houzi wong"] = {
 	trait : "Houzi Wong (+2 Dexterity, +1 Wisdom)\n\nMonkey's Paw: My Unarmed Strikes do 1d6 + DEX damage.\nMask of the Wild: I can attempt to hide even when you are only lightly obscured by foliage, heavy rain, falling snow, mist, and other natural phenomena.\nHybrid Creature: I count as a Humanoid and a Beast for the purpose of game effects, and can be affected anything that affects either.", 
 };
 WeaponsList["monkey paws"] = { 
-	regExpSearch : /onkey paws/i, 
+	regExpSearch : /monkey paws/i, 
 	name : "Monkey Paws", 
 	source : ["HB", 0], 
 	list : "melee", 
